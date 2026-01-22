2e61b452d17dfa50e49ccdd1258f39765d89f7c5
saikeerthiai <saikeerthinallamothu@gmail.com>
2026-01-22 10:07:37 +0530

    feat: initial-commit

---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,28 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args) {
+        byte age = 23;
+        short price = 2500;
+        int salary = 50000;
+        long phoneNumber = 230078930L;
+        System.out.println("Age : " + age);
+        System.out.println("price: " + price);
+        System.out.println("salary: " + salary);
+        System.out.println("phoneNumber: " + phoneNumber);
+        float rating = 4.5F;
+        System.out.println("rating : " + rating);
+        byte subjectMarks, standard, feverTemperature, levels, floor,steps;
+        subjectMarks = 89;
+        standard = 11;
+        feverTemperature =99;
+        levels = 5;
+        floor = 9;
+        steps = 25;
+        double annual = 133333334.3793;
+        char grade = 'A';
+        char grade1 = 97;
+       
+
+    }
+}
